fc0849c74c472427b96d448e4ad077a21343230c
Tom Bear <tom@bearjam.dev>
2021-05-03 13:29:14 +0100

    1

--- src/components/create/ThreeCanvasText.tsx
+++ src/components/create/ThreeCanvasText.tsx
@@ -1,13 +1,25 @@
 import React from "react"
 import { Text } from "@react-three/drei"
 import { CanvasTextItem } from "types/canvas"
+import { colors } from "tailwindcss/defaultTheme"
 
 type Props = {
   item: CanvasTextItem
 }
 
 const ThreeCanvasText = ({ item }: Props) => {
-  return <Text>{item.text}</Text>
+  // return <Text>{item.text}</Text>
+  return (
+    <Text
+      color={colors.red[600]}
+      fontSize={32}
+      lineHeight={undefined}
+      font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+      fillOpacity={1}
+    >
+      {item.text}
+    </Text>
+  )
 }
 
 export default ThreeCanvasText
