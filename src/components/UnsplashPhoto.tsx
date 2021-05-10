ca476cd34750188599056048906948e8aa06551c
Tom Bear <tom@bearjam.dev>
2021-05-10 14:16:43 +0100

    ^^

--- src/components/UnsplashPhoto.tsx
+++ src/components/UnsplashPhoto.tsx
@@ -4,22 +4,40 @@ import React from "react"
 import { UnsplashPhotoT } from "../types/unsplash"
 import Image from "./Image"
 
-type Props = Omit<ImageProps, "width" | "src"> & {
+type Props = {
   photo: UnsplashPhotoT
-  width?: number
-}
+  aspectRatio?: number
+  layout?: "intrinsic" | "fill" | "fixed" | "responsive"
+} & Partial<ImageProps>
 
 const UnsplashPhoto = ({
   photo,
   width = window?.innerWidth ?? 600,
+  height,
+  aspectRatio,
+  layout = "intrinsic",
   ...props
 }: Props) => {
-  const ratio = photo.height / photo.width
+  const ratio = aspectRatio ?? photo.height / photo.width
   const url = stringifyUrl({
     url: photo.urls.raw,
     query: { w: width },
   })
-  return <Image src={url} width={width} height={ratio * width} {...props} />
+  switch (layout) {
+    case "intrinsic":
+      return (
+        <Image
+          src={url}
+          width={width}
+          height={height ?? ratio * Number(width)}
+          {...props}
+        />
+      )
+    case "fill":
+      return <Image src={url} layout={layout} {...props} />
+    default:
+      return null
+  }
 }
 
 export default UnsplashPhoto
--- src/components/explore/topics/index.tsx
+++ src/components/explore/topics/index.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import useSWR from "swr"
 import Link from "components/Link"
-import UnsplashPhoto from "components/UnsplashPhoto"
+import NextImage from "next/image"
 import { fetcher } from "lib/util"
 import { UnsplashTopic } from "types/unsplash"
 import css from "./index.module.css"
+import UnsplashPhoto from "components/UnsplashPhoto"
 
 const Explore = () => {
   const { data, error } = useSWR<{ results: UnsplashTopic[]; total: number }>(
@@ -24,10 +25,13 @@ const Explore = () => {
             <a>
               <div>
                 <h2>{topic.title}</h2>
-                <UnsplashPhoto
-                  photo={topic.cover_photo!}
-                  className="max-h-12"
-                />
+                <div className="relative h-64 w-full">
+                  <UnsplashPhoto
+                    photo={topic.cover_photo!}
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
               </div>
             </a>
           </Link>
