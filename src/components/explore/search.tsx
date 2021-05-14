1641ea037bd71f667028beac0db1e4f0be47f3ca
Tom Bear <tom@bearjam.dev>
2021-05-14 15:44:38 +0100

    text form problem solved

--- src/components/explore/search.tsx
+++ src/components/explore/search.tsx
@@ -1,15 +1,19 @@
-import TextForm from "components/TextForm"
+import { zodResolver } from "@hookform/resolvers/zod"
+import Submit from "components/inputs/Submit"
+import TextInput from "components/inputs/TextInput"
 import UnsplashPhoto from "components/UnsplashPhoto"
 import { pipe } from "fp-ts/function"
 import { filter, map } from "fp-ts/ReadonlyArray"
 import { fetcher } from "lib/util"
 import { useRouter } from "next/router"
 import { stringifyUrl } from "query-string"
-import React from "react"
+import React, { HTMLProps } from "react"
+import { useForm } from "react-hook-form"
 import { usePhotoStore } from "stores/photos"
 import useSWR from "swr"
 import { Photos } from "unsplash-js/dist/methods/search/types/response"
 import shallow from "zustand/shallow"
+import * as z from "zod"
 
 const SearchResults = ({ query }: { query: string }) => {
   const [ids, dispatch] = usePhotoStore(
@@ -48,6 +52,34 @@ const SearchResults = ({ query }: { query: string }) => {
   )
 }
 
+type TextFormProps = Omit<HTMLProps<HTMLFormElement>, "onSubmit"> & {
+  onSubmit: (text: string) => any
+}
+
+const schema = z.object({
+  text: z.string().nonempty(),
+})
+
+const TextForm = ({ onSubmit, ...props }: TextFormProps) => {
+  const { register, handleSubmit, reset } = useForm({
+    defaultValues: {
+      text: "",
+    },
+    resolver: zodResolver(schema),
+  })
+
+  async function submitHandler({ text }: { text: string }) {
+    if (onSubmit) onSubmit(text)
+    reset()
+  }
+
+  return (
+    <form onSubmit={handleSubmit(submitHandler)} {...props}>
+      <TextInput name="text" ref={register} />
+      <Submit />
+    </form>
+  )
+}
 const ExploreSearch = () => {
   const router = useRouter()
   return (
