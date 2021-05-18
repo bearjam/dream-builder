8db4240f6fdd8721af1fa2df807408bf774d7543
Tom Bear <tom@bearjam.dev>
2021-05-18 12:32:20 +0100

    explore/search integrated

--- src/components/explore/index.tsx
+++ src/components/explore/index.tsx
@@ -1,18 +1,95 @@
+import React, { HTMLProps } from "react"
+import useSWR from "swr"
 import Link from "components/Link"
-import React from "react"
+import { fetcher } from "lib/util"
+import { UnsplashTopic } from "types/unsplash"
+import css from "./index.module.css"
+import UnsplashPhoto from "components/UnsplashPhoto"
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import Submit from "components/inputs/Submit"
+import TextInput from "components/inputs/TextInput"
+import { useForm } from "react-hook-form"
+import { useRouter } from "next/router"
 
-const IndexPage = () => {
+const schema = z.object({
+  text: z.string().nonempty(),
+})
+
+type TextFormProps = Omit<HTMLProps<HTMLFormElement>, "onSubmit"> & {
+  onSubmit: (text: string) => any
+}
+
+const TextForm = ({ onSubmit, ...props }: TextFormProps) => {
+  const { register, handleSubmit, reset } = useForm({
+    defaultValues: {
+      text: "",
+    },
+    resolver: zodResolver(schema),
+  })
+
+  async function submitHandler({ text }: { text: string }) {
+    if (onSubmit) onSubmit(text)
+    reset()
+  }
+
   return (
-    <article>
-      <h2>exploreIndex</h2>
-      <Link href="/explore/topics">
-        <a>Topics</a>
-      </Link>
-      <Link href="/explore/search">
-        <a>Search</a>
-      </Link>
-    </article>
+    <form onSubmit={handleSubmit(submitHandler)} {...props}>
+      <TextInput name="text" ref={register} />
+      <Submit />
+    </form>
+  )
+}
+const Explore = () => {
+  const router = useRouter()
+  const { data, error } = useSWR<{ results: UnsplashTopic[]; total: number }>(
+    `/api/unsplash/topics`,
+    fetcher
+  )
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  return (
+    <div className={css.root}>
+      <section>
+        <h1>Search</h1>
+        <TextForm
+          onSubmit={(text) =>
+            void router.push({
+              pathname: "/explore/search",
+              query: {
+                q: text,
+              },
+            })
+          }
+        />
+      </section>
+      <section>
+        <h1>Choose a collection</h1>
+        <div className={css.topicsContainer}>
+          {data.results.map((topic) => (
+            <div key={topic.id}>
+              <Link href={`/explore/topics/${topic.slug}`}>
+                <a>
+                  <div className={css.cover}>
+                    <h2>{topic.title}</h2>
+                    <div className={css.image}>
+                      <UnsplashPhoto
+                        photo={topic.cover_photo!}
+                        layout="fill"
+                        objectFit="cover"
+                      />
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
   )
 }
 
-export default IndexPage
+export default Explore
--- src/components/explore/search.tsx
+++ src/components/explore/search.tsx
@@ -1,19 +1,14 @@
-import { zodResolver } from "@hookform/resolvers/zod"
-import Submit from "components/inputs/Submit"
-import TextInput from "components/inputs/TextInput"
 import UnsplashPhoto from "components/UnsplashPhoto"
 import { pipe } from "fp-ts/function"
 import { filter, map } from "fp-ts/ReadonlyArray"
 import { fetcher } from "lib/util"
 import { useRouter } from "next/router"
 import { stringifyUrl } from "query-string"
-import React, { HTMLProps } from "react"
-import { useForm } from "react-hook-form"
+import React from "react"
 import { usePhotoStore } from "stores/photos"
 import useSWR from "swr"
 import { Photos } from "unsplash-js/dist/methods/search/types/response"
 import shallow from "zustand/shallow"
-import * as z from "zod"
 
 const SearchResults = ({ query }: { query: string }) => {
   const [ids, dispatch] = usePhotoStore(
@@ -52,48 +47,11 @@ const SearchResults = ({ query }: { query: string }) => {
   )
 }
 
-type TextFormProps = Omit<HTMLProps<HTMLFormElement>, "onSubmit"> & {
-  onSubmit: (text: string) => any
-}
-
-const schema = z.object({
-  text: z.string().nonempty(),
-})
-
-const TextForm = ({ onSubmit, ...props }: TextFormProps) => {
-  const { register, handleSubmit, reset } = useForm({
-    defaultValues: {
-      text: "",
-    },
-    resolver: zodResolver(schema),
-  })
-
-  async function submitHandler({ text }: { text: string }) {
-    if (onSubmit) onSubmit(text)
-    reset()
-  }
-
-  return (
-    <form onSubmit={handleSubmit(submitHandler)} {...props}>
-      <TextInput name="text" ref={register} />
-      <Submit />
-    </form>
-  )
-}
 const ExploreSearch = () => {
   const router = useRouter()
   return (
     <article>
       <h2>exploreSearch</h2>
-      <TextForm
-        onSubmit={(text) =>
-          void router.push({
-            query: {
-              q: text,
-            },
-          })
-        }
-      />
       {router.query?.q ? (
         <SearchResults query={router.query.q as string} />
       ) : null}
